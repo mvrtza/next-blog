847569542cf63b9ab6758e03398ef2907aa625fa
mvrtza <morixplus@gmail.com>
2025-05-07 06:33:00 +0400

    fixed slug error next.js 15

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
 import {ArrowLeftIcon} from "@heroicons/react/24/solid"
 import {getArticleData} from "@/lib/article";
+type Params = Promise<{ slug: string }>
+const Article = async ({ params }: { params: Params }) => {
+    const { slug } = await params;
 
-const Article = async ({params}:{params:{slug: string}})=>{
-    const articleData  = await getArticleData(params.slug)
+    const articleData  = await getArticleData(slug)
 
     return(<section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
         <div className="flex  justify-between font-poppins ">
