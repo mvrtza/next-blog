bcc64d5c076aba795f583f0dcbaf826927784f8f
mvrtza <morixplus@gmail.com>
2025-05-07 06:37:50 +0400

    test update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return(
       <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
   <header className="font-coromorantGaramond font-light text-6xl text-neutral-900 text-center">
-      <h1>minimal blog</h1>
+      <h1>morteza minimal blog</h1>
   </header>
           <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
               {articles !== null && Object.keys(articles).map((article)=><ArticleItemList category={article} articles={articles[article]} key={article}/>)}
